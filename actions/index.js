dee11e73ccd79000df430b37ba7088ed83037c0e
Bright Mahere <111682296+Brightechnee@users.noreply.github.com>
2024-02-08 23:00:11 +0530

    Create index.js

---
+++ actions/index.js
@@ -0,0 +1,15 @@
+// actions.js
+
+export const increment = (val) => {
+    return {
+        type: 'INCREMENT',
+        inc: val
+    };
+};
+
+export const decrement = (val) => {
+    return {
+        type: 'DECREMENT',
+        dec: val
+    };
+};
