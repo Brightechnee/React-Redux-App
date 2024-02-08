40cc5dd82874f5cf8205adf71f9acc954a87059a
Bright Mahere <111682296+Brightechnee@users.noreply.github.com>
2024-02-08 23:01:12 +0530

    Create index.js

---
+++ reducers/index.js
@@ -0,0 +1,19 @@
+//reducers
+import {combineReducers} from 'redux'
+
+const counter = (state=0,action)=>{
+    if(action.type === 'INCREMENT') {
+        //This will increase the value of counter by the value passed to the increment method
+        return state+action.inc;
+    }
+
+    if(action.type === 'DECREMENT') {
+        return state-action.dec;
+    }
+    //Returns the current value of the counter
+    return state;
+}
+
+const myReducers = combineReducers({counter});
+
+export default myReducers;
